c4d5448b5ac0c47ab223e1af3596bb8fe53b354d
kevin kimiti <kimitikevin@gmail.com>
2018-02-18 10:53:04 +0300

    first commit

---
+++ adding.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+ 
+ int main(void){
+     int a,b,c;
+     printf("enter two numbers");
+     scanf("%d %d",&a, &b);
+     c = a + b;
+     printf("the sum of the numbers is: %d",c);
+     return 0;
+ }
